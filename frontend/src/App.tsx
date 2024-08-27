a62d019cafb65a5e2e5dcc22f3e6fa91eda8bca6
Shashank <s_shashank@outlook.com>
2024-08-27 23:25:08 +0530

    setup frontend app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <h1 className="text-3xl text-pink-600">Expense Tracker</h1>
+        </>
+    );
+}
+
+export default App;
